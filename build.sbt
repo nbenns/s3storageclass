dd9e98c9f44e547ba370d35e336dbe5aa4e2b30b
Nigel Benns <nigel@500px.com>
2018-05-07 14:34:58 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "s3storageclass"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
+
+libraryDependencies ++= Seq(
+  "com.amazonaws" % "aws-java-sdk" % "1.11.321",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.12",
+  "com.lightbend.akka" %% "akka-stream-alpakka-s3" % "0.18"
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,73 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.alpakka.s3.impl.ListBucketVersion2
+import akka.stream.alpakka.s3.scaladsl.{ListBucketResultContents, S3Client}
+import akka.stream.alpakka.s3.{MemoryBufferType, S3Settings}
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.regions.DefaultAwsRegionProviderChain
+import com.amazonaws.services.s3.model._
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import scala.concurrent.{Future, blocking}
+import scala.language.postfixOps
+import scala.util.{Failure, Success}
+
+object Main extends App {
+  implicit val system = ActorSystem("s3storageclass")
+  implicit val ec = system.dispatcher
+  implicit val mat = ActorMaterializer()
+
+  val awsCredentialsProvider = new DefaultAWSCredentialsProviderChain()
+  val regionProvider = new DefaultAwsRegionProviderChain()
+
+  val settings = new S3Settings(
+    MemoryBufferType,
+    None,
+    awsCredentialsProvider,
+    regionProvider,
+    true,
+    None,
+    ListBucketVersion2
+  )
+
+  // We will use a streaming source for the directory listing
+  val s3Client: S3Client = new S3Client(settings)
+
+  // Copy objects must be done with the AWS S3Client
+  val awsS3Client: AmazonS3 = AmazonS3ClientBuilder.standard().build()
+
+  def updateStorageClass(listBucketResult: ListBucketResultContents): Future[(String, CopyObjectResult)] = {
+    val srcBucket = listBucketResult.bucketName
+    val srcKey = listBucketResult.key
+    val destBucket = srcBucket
+    val destKey = srcKey
+
+    val cor = new CopyObjectRequest(srcBucket, srcKey, destBucket, destKey)
+    cor.setStorageClass(StorageClass.Standard)
+
+    Future {
+      blocking {
+        val res = awsS3Client.copyObject(cor)
+        (destKey, res)
+      }
+    }
+  }
+
+  if (args.length == 0) {
+    println("please provide a name for the S3 bucket.")
+    system.terminate()
+  } else {
+    val bucketName = args(0)
+
+    s3Client.listBucket(bucketName, None)
+      .filter(_.storageClass == StorageClass.StandardInfrequentAccess.toString)
+      .mapAsyncUnordered(100) { listBucketResult => updateStorageClass(listBucketResult) }
+      .runForeach { case (key, res) => println(s"Updated storageClass on $key - ${res.getLastModifiedDate}") }
+      .onComplete { tryRes =>
+        tryRes match {
+          case Success(res) => println("All Done!")
+          case Failure(ex) => system.log.error(ex, "Failed :(")
+        }
+        system.terminate()
+      }
+  }
+}
