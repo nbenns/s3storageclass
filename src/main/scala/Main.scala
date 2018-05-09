cb5c1d7824e36306fcfabdaeb138203cc43fcbc9
Nigel Benns <nigel@500px.com>
2018-05-09 11:42:48 -0400

    Remove alpakka because it can't resume from a position if its crashes.
    Create an Iterable[S3ObjectSummary] to use as a Source for the stream.
    Side-effect its faster!

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,16 @@
 import akka.actor.ActorSystem
+import akka.stream.ActorAttributes.supervisionStrategy
 import akka.stream.ActorMaterializer
-import akka.stream.alpakka.s3.impl.ListBucketVersion2
-import akka.stream.alpakka.s3.scaladsl.{ListBucketResultContents, S3Client}
-import akka.stream.alpakka.s3.{MemoryBufferType, S3Settings}
+import akka.stream.Supervision.resumingDecider
+import akka.stream.scaladsl.Source
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.regions.DefaultAwsRegionProviderChain
 import com.amazonaws.services.s3.model._
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import scala.collection.JavaConverters._
 import scala.concurrent.{Future, blocking}
 import scala.language.postfixOps
-import scala.util.{Failure, Success}
-import akka.stream.ActorAttributes.supervisionStrategy
-import akka.stream.Supervision.resumingDecider
+import scala.util.{Failure, Success, Try}
 
 object Main extends App {
   implicit val system = ActorSystem("s3storageclass")
@@ -21,25 +20,61 @@ object Main extends App {
   val awsCredentialsProvider = new DefaultAWSCredentialsProviderChain()
   val regionProvider = new DefaultAwsRegionProviderChain()
 
-  val settings = new S3Settings(
-    MemoryBufferType,
-    None,
-    awsCredentialsProvider,
-    regionProvider,
-    true,
-    None,
-    ListBucketVersion2
-  )
-
-  // We will use a streaming source for the directory listing
-  val s3Client: S3Client = new S3Client(settings)
-
   // Copy objects must be done with the AWS S3Client
   val awsS3Client: AmazonS3 = AmazonS3ClientBuilder.standard().build()
 
-  def updateStorageClass(listBucketResult: ListBucketResultContents): Future[(String, CopyObjectResult)] = {
-    val srcBucket = listBucketResult.bucketName
-    val srcKey = listBucketResult.key
+  def s3Iterable(bucketName: String, initialStartAfter: Option[String]): scala.collection.immutable.Iterable[S3ObjectSummary] = new scala.collection.immutable.Iterable[S3ObjectSummary] {
+    override def iterator = new Iterator[S3ObjectSummary] {
+      var items: List[S3ObjectSummary] = List.empty
+      var continuationToken: Option[String] = Some(null)
+      var startAfter: Option[String] = initialStartAfter
+
+      def getMore = {
+        def getMoreWithRetry(retry: Int = 3): Try[ListObjectsV2Result] = {
+          val lor = new ListObjectsV2Request()
+          lor.setBucketName(bucketName)
+
+          if (startAfter.nonEmpty) {
+            lor.setStartAfter(startAfter.get)
+            startAfter = None
+          } else if (continuationToken.nonEmpty) {
+            lor.setContinuationToken(continuationToken.get)
+          }
+
+          Try(awsS3Client.listObjectsV2(lor))
+            .recoverWith {
+              case _ if retry > 0 => getMoreWithRetry(retry - 1)
+              case ex => Failure(ex)
+            }
+        }
+
+        getMoreWithRetry().foreach { res =>
+          continuationToken = Option(res.getContinuationToken)
+          items = items ++ res.getObjectSummaries.asScala.toList
+        }
+      }
+
+      override def hasNext = {
+        if (items.isEmpty && continuationToken.nonEmpty ) getMore
+
+        items.nonEmpty
+      }
+
+      override def next() = {
+        if (items.isEmpty && continuationToken.nonEmpty ) getMore
+
+        val head = items.head
+        val tail = items.tail
+        items = tail
+
+        head
+      }
+    }
+  }
+
+  def updateStorageClass(objectSummary: S3ObjectSummary): Future[(String, CopyObjectResult)] = {
+    val srcBucket = objectSummary.getBucketName
+    val srcKey = objectSummary.getKey
     val destBucket = srcBucket
     val destKey = srcKey
     val cor = new CopyObjectRequest(srcBucket, srcKey, destBucket, destKey)
@@ -68,9 +103,10 @@ object Main extends App {
     system.terminate()
   } else {
     val bucketName = args(0)
+    val startAfter = Try { args(1) }.toOption
 
-    s3Client.listBucket(bucketName, None)
-      .filter(_.storageClass == StorageClass.StandardInfrequentAccess.toString)
+    Source(s3Iterable(bucketName, startAfter))
+      .filter(_.getStorageClass == StorageClass.StandardInfrequentAccess.toString)
       .mapAsyncUnordered(25)(updateStorageClass)
       .withAttributes(supervisionStrategy(resumingDecider))
       .runForeach { case (key, res) => println(s"Updated storageClass on $key - ${res.getLastModifiedDate}") }
